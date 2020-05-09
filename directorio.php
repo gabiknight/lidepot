7935c30991071e8f8bbe658bb1fa99b04753f1e0
gabiknight <39223734+gabiknight@users.noreply.github.com>
2020-05-09 17:56:05 -0300

    corregido listar archivos
    al listar archivos, se corrigió que apunten a archivos/carpeta.personal

--- directorio.php
+++ directorio.php
@@ -8,10 +8,10 @@ while($elemento = readdir($directorio))
 {
     if($elemento != '.' && $elemento != '..'){
         if(is_dir("archivos/".$elemento)){
-            $listar .= "<li><a href='archivos/$elemento' target='_blank'>$elemento/</a></li>";            
+            $listar .= "<li><a href='archivos/$carpeta/$elemento' target='_blank'>$elemento/</a></li>";            
         }
         else{
-            $listar .= "<li><a href='archivos/$elemento' target='_blank'>$elemento</a></li>";
+            $listar .= "<li><a href='archivos/$carpeta/$elemento' target='_blank'>$elemento</a></li>";
         }
     }
 }
